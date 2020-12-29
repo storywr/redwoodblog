4dbbd5f040b2b357cee548aec20c67c776b435d0
storywr <storywr@gmail.com>
2020-12-29 14:07:11 -0500

    create blog

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,101 @@
+import {
+  FieldError,
+  Form,
+  FormError,
+  Label,
+  Submit,
+  TextAreaField,
+  TextField
+} from '@redwoodjs/forms'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
+import { useForm } from 'react-hook-form'
+
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
+const ContactPage = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+  const { addMessage } = useFlash()
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      addMessage('Thank you for your submission!', {
+        style: { backgroundColor: 'green', color: 'white', padding: '1rem' }
+      })
+      formMethods.reset()
+    }
+  })
+
+  const handleSubmit = data => create({ variables: { input: data }})
+
+  return (
+    <BlogLayout>
+      <Flash timeout={2000} />
+      <Form
+        onSubmit={handleSubmit}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+        />
+        <Label
+          name='name'
+          errorClassName='error'
+        >
+          Name
+        </Label>
+        <TextField
+          name='name'
+          validation={{ required: true }}
+          errorClassName='error'
+        />
+        <FieldError name='name' className='error' />
+
+        <Label
+          name='email'
+          errorClassName='error'
+        >
+          Email
+        </Label>
+        <TextField
+          name='email'
+          validation={{
+            required: true,
+            // pattern: {
+            //   value: /[^@]+@[^.]+\..+/,
+            //   message: 'Please enter a valid email address',
+            // }
+          }}
+          errorClassName='error'
+        />
+        <FieldError name='email' className='error' />
+
+        <Label
+          name='message'
+          errorClassName='error'
+        >
+          Message
+        </Label>
+        <TextAreaField
+          name='message'
+          validation={{ required: true }}
+          errorClassName='error'
+        />
+        <FieldError name='message' className='error' />
+
+        <Submit disabled={loading}>Save</Submit>
+      </Form>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
